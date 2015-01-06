56c6f04b28706d94465ec4c3f62837e33eb81377
Joe Love <joe@lemondigits.com>
2015-01-06 23:37:37 +0000

    Added step definitions for message view page

---
+++ features/step_definitions/messages_steps.rb
@@ -0,0 +1,33 @@
+Given(/^I am on the message display page$/) do
+  visit '/messages/index'
+end
+
+Then(/^I should see no messages$/) do
+  expect(page).to_not have_css('.message')
+end
+
+When(/^I click the "([^"]+?)" button$/) do |button_text|
+  click_button button_text
+end
+
+Then(/^I should see some messages with handle and sentiment information$/) do
+  expect(page).to have_css('.message .message__value')
+  expect(page).to have_css('.message .message__handle')
+  expect(page).to have_css('.message .message__sentiment')
+end
+
+When(/^I click the "([^"]+?)" button twice$/) do |button_text|
+  2.times { click_button button_text }
+end
+
+Then(/^I should see a message with a message counter of "([1-9][0-9]*)"$/) do |count|
+  expect(page).to have_css('.message .message__counter', text: count)
+end
+
+Then(/^I should see a message containing "([^"]+?)" that is highlighted$/) do |text|
+  expect(page).to have_css('.message.highlight', text: text)
+end
+
+Then(/^I should see a message not containing "([^"]+?)" that is not highlighted$/) do |text|
+  expect(page).to_not have_css('.message.highlight', text: /^((?!#{text}).)*$/)
+end
