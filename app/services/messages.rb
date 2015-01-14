f9fc7f282028c472d89c469d864f69813b661d14
Joe Love <joe@lemondigits.com>
2015-01-14 18:19:36 +0000

    Refactored Messages class

--- app/services/messages.rb
+++ app/services/messages.rb
@@ -2,13 +2,11 @@ require 'open-uri'
 require 'execjs'
 
 class Messages
+  attr_reader :response, :message
+
   def fetch
-    response = fetch_new
-    unless is_error(response)
-      save_messages(response)
-    else
-      response['error']['message']
-    end
+    fetch_messages
+    save_messages
   end
 
   def list
@@ -17,39 +15,58 @@ class Messages
 
   private
 
-  def save_messages(response)
-    messages = relevant_messages_for(response)
-    messages.each do |message|
-      existing_message = Message.find_by(id: message['id'])
-      if existing_message
-        existing_message.increment!(:count)
-      else
-        Message.create(message.merge(count: 1))
-      end
+  def fetch_messages
+    @response = ExecJS.eval(response_body)
+  end
+
+  def save_messages
+    unless is_error
+      add_messages
+    else
+      response['error']['message']
     end
-  end
-
-  def relevant_messages_for(response)
-    response.select { |item| relevant? item }
-  end
-
-  def is_error(response)
-    response.is_a?(Hash) && response.has_key?('error')
-  end
-
-  def relevant?(item)
-    item['message'].match(/(?:diet\s+)?(?:coke|coca(?:-|\s+)cola)/i)
-  end
-
-  def fetch_new
-    ExecJS.eval(response_body)
   end
 
   def response_body
     make_request.read
   end
 
+  def is_error
+    response.is_a?(Hash) && response.has_key?('error')
+  end
+
+  def add_messages
+    relevant_messages.each do |message|
+      @message = message
+      add_or_update
+    end
+  end
+
   def make_request
     open(Rails.application.config.messages_api_path)
+  end
+
+  def relevant_messages
+    response.select { |item| relevant? item }
+  end
+
+  def add_or_update
+    if existing_message
+      existing_message.increment!(:count)
+    else
+      add_message
+    end
+  end
+
+  def existing_message
+    Message.find_by(id: message['id'])
+  end
+
+  def add_message
+    Message.create(message.merge(count: 1))
+  end
+
+  def relevant?(item)
+    item['message'].match(/(?:diet\s+)?(?:coke|coca(?:-|\s+)cola)/i)
   end
 end
